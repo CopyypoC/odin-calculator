7f8a4f69c6ceaed532f0d5148d1662942944232c
CopyypoC <william.wang650@gmail.com>
2024-04-11 20:56:05 -0700

    Add basic math operations

---
+++ scripts/calculator.js
@@ -0,0 +1,45 @@
+const input = document.querySelector('#input');
+const keypadContainer = document.querySelector('#keypad-container');
+const keyClear = document.querySelector('#key-clear');
+let operand1;
+let operand2;
+let operator;
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, operand1, operand2) {
+    switch (operator) {
+        case '+':
+            return add(operand1, operand2);
+        case '-':
+            return subtract(operand1, operand2);
+        case '*':
+            return multiply(operand1, operand2);
+        case '/':
+            return divide(operand1, operand2);
+    }
+}
+
+// keypadContainer.addEventListener('click', (e) => {
+//     // Populate display
+//     input.textContent += e.target.textContent;
+//     // When operator is clicked
+// });
+
+// keyClear.addEventListener('click', () => {
+//     input.textContent = '';
+// });
